6f4bf8bda1b7e1c090d2153c845f049cf45d0d3c
JoeiLin <83330368+JoeiLin@users.noreply.github.com>
2021-04-28 17:16:40 +1000

    Update AppBar.dart

--- AppBar.dart
+++ AppBar.dart
@@ -1,33 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:project1/menu.dart';
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Container(
-                backgroundColor: Colors.lightBlueAccent[50],
-                appBar: myAppBar("Coach-Kit"),
-                //set app bar from appbar.dart
-                // use like this where ever you
-                drawer: myDrawer()
-              ),
-  }
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 Widget myAppBar(String title){
   return AppBar(
