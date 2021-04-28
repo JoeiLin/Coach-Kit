cdf83891b82dd118e29a8ebfda7a30c372bd82ae
JoeiLin <83330368+JoeiLin@users.noreply.github.com>
2021-04-28 15:56:25 +1000

    Add files via upload

---
+++ Menu.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget myDrawer(){
+  return Drawer(
+    child: SingleChildScrollView(
+        child:Container(
+            margin:EdgeInsets.only(top:50),
+            child:Column(children: <Widget>[
+
+              ListTile(
+                  leading:Icon(Icons.home),
+                  title:Text("Home"),
+                  onTap:(){
+                    // Home button action
+                  }
+              ),
+
+              ListTile(
+                  leading:Icon(Icons.person),
+                  title:Text("My Profile"),
+                  onTap:(){
+                    // My Pfofile button action
+                  }
+              ),
+
+              ListTile(
+                  leading:Icon(Icons.logout),
+                  title:Text("Log Out"),
+                  onTap:(){
+
+                  }
+              )
+
+              //add more drawer menu here
+
+            ],)
+        )
+    ),
+  );
+}
